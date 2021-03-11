cad9ac9cc8a918223387fc356041aae70a05fd2d
HkopMD <hkopmdgt@protonmail.ch>
2021-03-11 19:29:10 +0000

    Update Bandtest.sh

--- Bandtest.sh
+++ Bandtest.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-init="$(($(cat	/sys/class/net/[ew]*/statistics/rx_bytes | paste -sd '+')))"
+init="$(($(cat    /sys/class/net/[ew]*/statistics/rx_bytes | paste -sd '+')))"
 
 printf "Wirte the web address: "
 
 read -r lol
 
-fin="$(($(cat	/sys/class/net/[ew]*/statistics/rx_bytes | paste -sd '+')))"
+fin="$(($(cat    /sys/class/net/[ew]*/statistics/rx_bytes | paste -sd '+')))"
 
-printf "%4sB of bandwidth used.\\n" $(numfmt --to=iec $(($fin-$init))) 
+printf "Bandwidth used:%4sB \\n" $(numfmt --to=iec $(($fin-$init))) 
